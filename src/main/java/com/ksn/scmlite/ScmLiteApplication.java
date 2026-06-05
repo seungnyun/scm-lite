8cff4fe15037fcb4d54eeed9564ebe67510f3e9f
KSN <vas0707@naver.com>
2026-06-05 14:14:07 +0900

    Project Create_20260605

---
+++ src/main/java/com/ksn/scmlite/ScmLiteApplication.java
@@ -0,0 +1,13 @@
+package com.ksn.scmlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScmLiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksn/scmlite/ScmLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksn.scmlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmLiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
