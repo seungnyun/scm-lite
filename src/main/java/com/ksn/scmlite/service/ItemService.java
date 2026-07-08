04dab58191cb09ea0ba8531d228e99d3e11c53ed
KSN <vas0707@naver.com>
2026-07-08 09:23:18 +0900

    add businessException

--- src/main/java/com/ksn/scmlite/service/ItemService.java
+++ src/main/java/com/ksn/scmlite/service/ItemService.java
@@ -1,8 +1,10 @@
 package com.ksn.scmlite.service;
 
 import com.ksn.scmlite.entity.Item;
+import com.ksn.scmlite.exception.BusinessException;
 import com.ksn.scmlite.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +21,7 @@ public class ItemService {
     }
 
     public Item findbyid(Long id){
-        return  itemRepository.findById(id).orElseThrow(() -> new RuntimeException("Item not found"));
+        return  itemRepository.findById(id).orElseThrow(() -> new BusinessException(HttpStatus.NOT_FOUND, "품목을 찾을 수 없습니다."));
     }
 
     public Item save(Item item) {
@@ -32,7 +34,7 @@ public class ItemService {
 
     @Transactional
     public Item update(Long id, Item request){
-        Item item = itemRepository.findById(id).orElseThrow(() -> new RuntimeException("Item not found"));
+        Item item = itemRepository.findById(id).orElseThrow(() -> new BusinessException(HttpStatus.NOT_FOUND, "품목을 찾을 수 없습니다."));
 
         item.update(request.getItemName(), request.getSafetyStock());
 
--- src/main/java/com/ksn/scmlite/service/WarehouseService.java
+++ src/main/java/com/ksn/scmlite/service/WarehouseService.java
@@ -1,8 +1,10 @@
 package com.ksn.scmlite.service;
 
 import com.ksn.scmlite.entity.Warehouse;
+import com.ksn.scmlite.exception.BusinessException;
 import com.ksn.scmlite.repository.WarehouseRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +21,7 @@ public class WarehouseService {
     }
 
     public Warehouse findById(Long id){
-        return warehouseRepository.findById(id).orElseThrow(() -> new RuntimeException("Warehouse not found"));
+        return warehouseRepository.findById(id).orElseThrow(() -> new BusinessException(HttpStatus.NOT_FOUND, "창고를 찾을 수 없습니다."));
     }
 
     public Warehouse save(Warehouse warehouse){
@@ -32,7 +34,7 @@ public class WarehouseService {
 
     @Transactional
     public Warehouse update(Long id, Warehouse request){
-        Warehouse warehouse = warehouseRepository.findById(id).orElseThrow(() -> new RuntimeException("Warehouse not found"));
+        Warehouse warehouse = warehouseRepository.findById(id).orElseThrow(() -> new BusinessException(HttpStatus.NOT_FOUND, "창고를 찾을 수 없습니다."));
 
         warehouse.update(request.getWarehouseName());
 
